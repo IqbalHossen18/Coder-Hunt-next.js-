4afa72a68cd832c8548a68b6cc8c27f4b1345582
Iqbal <iqbalhossen1418@gmail.com>
2023-11-11 14:35:11 +0600

    after bootstrap navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,28 +1,50 @@
 import React from 'react'
 import Link from 'next/link'
-import { useRouter } from 'next/router'
-// import { usePathname } from 'next/navigation'
+import { usePathname } from 'next/navigation'
 const Navbar = () => {
-    const {pathname} = useRouter()
+
+    let pathname = usePathname()
 
     return (
         <>
-            <style jsx>
+            <nav id='navbar' className="navbar sticky-top navbar-expand-lg bg-dark navbar-dark">
+                <div className="container-fluid">
+                    <Link className="navbar-brand" href="/">Coder Hunt</Link>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                            <li className="nav-item">
+                                <Link className={`nav-link ${ pathname === '/' ? 'active':''}`} aria-current="page" href="/">Home</Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link className={`nav-link ${ pathname === '/about' ? 'active':''}`} href="/about">About</Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link className={`nav-link ${ pathname === '/blog' ? 'active':''}`} href="/blog">Blog</Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link className={`nav-link ${ pathname === '/contact' ? 'active':''}`} href="/contact">Contact</Link>
+                            </li>
+                            
+                        </ul>
+                       
+                    </div>
+                </div>
+            </nav>
+            {/* <style jsx>
                 {`
            
                             nav {
                                 background-color: bisque;
                                 width:100%;
                                 display: flex;
+                                padding-top:12px;
                                 justify-content: center;
                                 align-items: center;
                                 height: 65px;
                                 border-bottom: 1px solid rgb(204, 202, 202);
-                            }
-                            @media screen and (max-width: 500px) {
-                                nav{
-                                    display:none;
-                                }
                             }
                             nav ul{
                                 display: flex;
@@ -61,7 +83,7 @@ const Navbar = () => {
                         <li>Contact</li>
                     </Link>
                 </ul>
-            </nav>
+            </nav> */}
         </>
     )
 }
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,13 +1,15 @@
 import Navbar from '@/components/Navbar'
 import '@/styles/globals.css'
 import Head from 'next/head';
+import Script from 'next/script';
 export default function App({ Component, pageProps }) {
   return <>
-  <Head>
-        <title>Coder Hunt</title>
-        <meta name="description" content="Your custom description here." />
-      </Head>
-    <Navbar/>
-   <Component {...pageProps} />
+    <Head>
+      <title>Coder Hunt</title>
+      <meta name="description" content="Your custom description here." />
+    </Head>
+    <Script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous" />
+    <Navbar />
+    <Component {...pageProps} />
   </>
 }
